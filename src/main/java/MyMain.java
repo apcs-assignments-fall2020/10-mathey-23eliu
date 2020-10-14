8e23807667bacbce1a6f160bf4c8cee1ecc59de6
23eliu <23eliu@athenian.org>
2020-10-14 16:30:41 +0000

    finished warm-up

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,13 +1,26 @@
-import java.util.Scanner;
-
 public class MyMain {
 
-    public static int someMethod() {
-        return 1;
+    public static int randomNumber() {
+        return (int) ((20 - 10 + 1) * Math.random()) + 10;
+    }
+
+    public static int largest(int a, int b, int c) {
+        int temp = Math.max(a, b);
+        return Math.max(temp, c);
+    }
+
+    public static int smallest(int a, int b, int c) {
+        int temp = Math.min(a, b);
+        return Math.min(temp, c);
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = randomNumber();
+        int b = randomNumber();
+        int c = randomNumber();
+        System.out.println("The three random numbers are: " + a + ", " + b + ", " + c + ".");
+        System.out.println("The largest number is " + largest(a, b, c));
+        System.out.println("The smallest number is " + smallest(a, b, c));
     }
 
 }
